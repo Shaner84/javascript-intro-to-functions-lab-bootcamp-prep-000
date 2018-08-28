3102e8ccd45082269bf508a993e853da725d8068
sreddy3@cox.net Reddy <sreddy3@cox.net>
2018-08-28 02:08:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,19 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  console.log (string).toUpperCase()
+  console.log(string.toUpperCase() )
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase() )
+}
+
+function sayHiToGrandma(string) {
+  console.log(string.toLowerCase() );
+  return 'I can\'t hear you!'
+}
+function sayHiToGrandma(string) {
+  return 'YES INDEED!'
+}
+function sayHiToGrandma(string) {
+  return 'I love you too'
 }
